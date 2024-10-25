5227fe4e5923152e9dbf82d253671ea8008e5a89
PiyushShri9990 <piyush.a.shri@gmail.com>
2024-10-25 18:59:33 +0530

    todo backed index.js updated

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
 import Connection from './database/db.js';
+import Routes from './routes/route.js';
 
 const app = express();
 
-const PORT = 5500;
+
+app.use(cors());
+
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/', Routes);
+
+const PORT = 8000;
 
 Connection();
 
-app.listen(PORT, ()=>console.log(`Your server is running successfully on port ${PORT}`));
+app.listen(PORT, () => console.log(`Your server is running successfully on PORT ${PORT}`));
